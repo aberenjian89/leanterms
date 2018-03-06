0e2e847490dbd33192f2ebe25d852cad63990cef
Akram <azaiezakrem@gmail.com>
2018-03-05 19:19:33 -0800

    added store

---
+++ frontend/store/store.js
@@ -0,0 +1,17 @@
+import {createStore,applyMiddleware} from 'redux';
+import RootReducer from '../reducers/root_reducer';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+
+
+const configureStore=(preloadedstate={})=>(
+    createStore(
+        RootReducer,
+        preloadedstate,
+        applyMiddleware(thunk,logger)
+    )
+);
+
+
+export default configureStore;
