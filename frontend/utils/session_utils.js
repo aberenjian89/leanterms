de6f4207c337a56ce542c411f62e4908f2a2f7bc
Ali Berenjian <Ali.rberenjian@gmail.com>
2018-03-07 14:42:11 -0800

    merge

--- frontend/utils/session_utils.js
+++ frontend/utils/session_utils.js
@@ -1,20 +1,29 @@
-export const CreateUser = (user) =>
+export const CreateUser = (user) =>(
   $.ajax({
       method: 'POST',
       url: '/api/signup',
       data: user
-  });
+  })
+);
 
-export const Logout = () =>
+export const Logout = () =>(
     $.ajax({
         method: 'GET',
         url: '/api/logout'
-    });
+    })
+);
 
-
-export const Login = (user) =>
+export const Login = (user) =>(
     $.ajax({
         method: 'POST',
         url: '/api/login',
         data: user
-    });
+    })
+);
+
+export const CurrentUser = () =>(
+    $.ajax({
+        method: 'GET',
+        url: '/api/current_user'
+    })
+);
