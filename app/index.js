a5301c1d6b32ee3362dbaf6a0ea01c36a2d7fa9c
Ali Berenjian <Ali.rberenjian@gmail.com>
2018-03-11 12:15:34 -0700

    Fix Heroku push

--- app/index.js
+++ app/index.js
@@ -53,7 +53,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../','index.html'));
 });
 
-let port = process.env.port;
+let port =  process.env.PORT;
 
 app.listen(port,function(err){
     console.log("Running Server on port",port);
