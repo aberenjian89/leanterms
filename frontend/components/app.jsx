7ac93b943f1507c29ef6511893f2ba8df5e7da8b
Akram <azaiezakrem@gmail.com>
2018-03-10 18:19:47 -0800

    added user auth requirement

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import NavbarContainter from './navbar/navbar_container';
 import Splash from './homepage/splash';
+import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
+import ContractShowContainer from './contract/contract_show_container';
+import ContractFormContainer from './contract_form_container';
 
 
 class App extends React.Component {
@@ -15,8 +18,10 @@ class App extends React.Component {
       <div className="app-div-wrapper">
           <Route path="/" component={NavbarContainter}/>
           <Switch>
-            <Route exact path="/" component={Splash} />
-
+            <AuthRoute exact path="/" component={Splash} />
+            <ProtectedRoute path="/contract/:id" component={ContractShowContainer}/>
+            <ProtectedRoute path="/contract" component={ContractFormContainer} />
+            <Redirect to="/" />
           </Switch>
       </div>
     );
