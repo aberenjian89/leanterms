9bd2e1e1f96e1a0762df4f0be15d2beba58b2193
Ali Berenjian <Ali.rberenjian@gmail.com>
2018-03-11 11:50:28 -0700

    Fix heroku push

--- app/index.js
+++ app/index.js
@@ -8,26 +8,34 @@ const session = require('express-session');
 const PdfGenerate = require('./routes/generatepdfroute');
 const authRouter = require('./routes/auth_routes');
 const contractRouter = require('./routes/contract_routes');
+const config = require('./config/config');
 
-let host;
+
+
+let env;
 if (process.env.NODE_ENV === 'production'){
-    host = "ec2-54-243-142-182.compute-1.amazonaws.com"
+    env = config.production;
 }else{
-    host = "localhost"
+    env = config.development;
 }
 
 
-const sequelize = new Sequelize('leanterms','','',{
-   host: host,
-   dialect: 'postgres',
-    operatorsAliases: false,
-    pool:{
-       max: 5,
-       min: 0,
-       acquire: 30000,
-       idle: 10000
-   }
-});
+// const sequelize = new Sequelize('d29gjgg23v8m8e','','',{
+//    host: host,
+//    dialect: 'postgres',
+//     operatorsAliases: false,
+//     pool:{
+//        max: 5,
+//        min: 0,
+//        acquire: 30000,
+//        idle: 10000
+//    }
+// });
+
+const sequelize = new Sequelize("postgres://kalgjzexwlzxic:9e2e4f63997f8e3cbf0210be372b2fe040c9967a7b6ca43e70ebbd0ac23db132@ec2-54-243-142-182.compute-1.amazonaws.com:5432/d29gjgg23v8m8e");
+
+
+
 // Testing Connection For Database
 sequelize.authenticate().then(() =>{
     console.log('Connection has been established successfully.');
@@ -59,6 +67,5 @@ app.listen(port,function(err){
     console.log("Running Server on port",port);
 });
 
-module.exports = require('../node_modules/.bin/sequelize');
 
 
